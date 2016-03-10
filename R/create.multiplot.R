d46dffb04bf4cd5644fd370616fb921165b7cfe9
Natalie Fox <nfox@mednet.ucla.edu>
2016-03-10 00:42:50 +0000

    re-worked version of create.multiplot, hopefully limit and spacing issues are fixed
    git-svn-id: https://code.boutroslab.oicr.on.ca/svn/BoutrosLab@51769 aa8168d8-c5e5-4185-88a0-f3e152ed764a

--- R/create.multiplot.R
+++ R/create.multiplot.R
@@ -10,14 +10,14 @@
 # credit be given to OICR scientists, as scientifically appropriate.
 
 ### FUNCTION TO CREATE MULTIPLOT ###################################################################
-create.multiplot <- function(plot.objects, filename = NULL, panel.heights = c(1,1), panel.widths = 1, main = NULL, main.just = 'center', main.x = 0.5, main.y = 0.5, main.cex = 3, main.key.padding = 1, ylab.padding = 5, xlab.padding = 5, xlab.to.xaxis.padding = 2, right.padding = 1, left.padding = 1, top.padding = 0.5, bottom.padding = 0.5, xlab.label = NULL, ylab.label = NULL, xlab.cex = 2, ylab.cex = 2,xlab.top.label = NULL,xlab.top.cex = 2, xlab.top.col = 'black', xlab.top.just = "center",xlab.top.x = 0.5, xlab.top.y = 0, xaxis.cex = 1.5, yaxis.cex = 1.5, xaxis.labels = TRUE, yaxis.labels = TRUE, xaxis.alternating = 1, yaxis.alternating = 1, xat = TRUE, yat = TRUE, xlimits = NULL, ylimits = NULL, xaxis.rot = 0, xaxis.fontface = 'bold', yaxis.fontface = 'bold', x.spacing = 1, y.spacing = 1, x.relation = 'same', y.relation = 'same', xaxis.tck = c(0.75,0.75), yaxis.tck = c(0.75,0.75), axes.lwd = 1.5, key.right.padding = 1, key.left.padding = 1, key.bottom.padding = 1, height = 6, width = 6, size.units = 'in', resolution = 1600, enable.warnings = FALSE, key = list(text = list(lab = c(''))), legend =  NULL, print.new.legend = FALSE, merge.legends = FALSE, plot.layout = c(1,length(plot.objects)), layout.skip=rep(FALSE,length(plot.objects)), description = 'Created with BoutrosLab.plotting.general', retrieve.plot.labels = FALSE, plot.labels.to.retrieve = NULL, style = 'BoutrosLab', remove.all.border.lines = FALSE, preload.default = 'custom') {
+create.multiplot <- function(plot.objects, filename = NULL, panel.heights = c(1,1), panel.widths = 1, main = NULL, main.just = 'center', main.x = 0.5, main.y = 0.5, main.cex = 3, main.key.padding = 1, ylab.padding = 5, xlab.padding = 5, xlab.to.xaxis.padding = 2, right.padding = 1, left.padding = 1, top.padding = 0.5, bottom.padding = 0.5, xlab.label = NULL, ylab.label = NULL, xlab.cex = 2, ylab.cex = 2,xlab.top.label = NULL,xlab.top.cex = 2, xlab.top.col = 'black', xlab.top.just = "center",xlab.top.x = 0.5, xlab.top.y = 0, xaxis.cex = 1.5, yaxis.cex = 1.5, xaxis.labels = TRUE, yaxis.labels = TRUE, xaxis.alternating = 1, yaxis.alternating = 1, xat = TRUE, yat = TRUE, xlimits = NULL, ylimits = NULL, xaxis.rot = 0, xaxis.fontface = 'bold', yaxis.fontface = 'bold', x.spacing = 1, y.spacing = 1, x.relation = 'same', y.relation = 'same', xaxis.tck = c(0.75,0.75), yaxis.tck = c(0.75,0.75), axes.lwd = 1.5, key.right.padding = 1, key.left.padding = 1, key.bottom.padding = 1, height = 6, width = 6, size.units = 'in', resolution = 1600, enable.warnings = FALSE, key = list(text = list(lab = c(''))), legend =  NULL, print.new.legend = FALSE, merge.legends = FALSE, plot.layout = c(1,length(plot.objects)), layout.skip=rep(FALSE,length(plot.objects)), description = 'Created with BoutrosLab.plotting.general', retrieve.plot.labels = FALSE, plot.labels.to.retrieve = NULL, style = 'BoutrosLab', remove.all.border.lines = FALSE, preload.default = 'custom', plot.for.carry.over.when.same = 1) {
 
-        if(preload.default == 'paper'){
+	if(preload.default == 'paper'){
 
-                }
-        else if(preload.default == 'web'){
+		}
+	else if(preload.default == 'web'){
 
-                }
+		}
 	# check that plots are trellis objects
 	for (i in 1:length(plot.objects)) {
 		if (class(plot.objects[[i]]) != 'trellis') {
@@ -25,11 +25,32 @@ create.multiplot <- function(plot.objects, filename = NULL, panel.heights = c(1,
 			}
 		}
 
+	# Collect the axis limites, labels and at from all the plots
+	xaxis.labels.plots <- list()[1:length(plot.objects)];
+	yaxis.labels.plots <- list()[1:length(plot.objects)];
+	xat.plots <- list()[1:length(plot.objects)];
+	yat.plots <- list()[1:length(plot.objects)];
+	xlimits.plots <- list()[1:length(plot.objects)];
+	ylimits.plots <- list()[1:length(plot.objects)];
+
 	# combine plot objects together and set layout
 	combined.plot.objects <- c(plot.objects[[1]], layout = plot.layout);
-        if(length(plot.objects) > 1) {
-		for (i in 2:length(plot.objects)) {
-			combined.plot.objects <- c(combined.plot.objects, plot.objects[[i]], layout = plot.layout, merge.legends = merge.legends);
+	if(length(plot.objects) > 1) {
+		for (i in 1:length(plot.objects)) {
+			if(i > 1) {
+				combined.plot.objects <- c(combined.plot.objects, plot.objects[[i]], layout = plot.layout, merge.legends = merge.legends);
+				}
+			# record each plot's axis values
+			if(length(plot.objects[[i]]$x.scales$labels) > 0) {
+				xaxis.labels.plots[[i]] <- plot.objects[[i]]$x.scales$labels;
+				}
+			if(length(plot.objects[[i]]$y.scales$labels) > 0) {
+				yaxis.labels.plots[[i]] <- plot.objects[[i]]$y.scales$labels;
+				}
+		       	xat.plots[[i]] <- plot.objects[[i]]$x.scales$at;
+		       	yat.plots[[i]] <- plot.objects[[i]]$y.scales$at;
+			xlimits.plots[[i]] <- plot.objects[[i]]$x.limits;
+		       	ylimits.plots[[i]] <- plot.objects[[i]]$y.limits;
 			}
 		}
 
@@ -38,6 +59,46 @@ create.multiplot <- function(plot.objects, filename = NULL, panel.heights = c(1,
 	else if (1 == xaxis.alternating) { xaxis.tck[2] <- 0; }
 	else if (2 == xaxis.alternating) { xaxis.tck[1] <- 0; }
 
+	# specify tck marks for different alternating settings
+	if (0 == yaxis.alternating) { yaxis.tck <- c(0,0); }
+	else if (1 == yaxis.alternating) { yaxis.tck[2] <- 0; }
+	else if (2 == yaxis.alternating) { yaxis.tck[1] <- 0; }
+
+	# if there are axis labels or tck locations for each plot, then the relations need to be free
+	if ((typeof(yaxis.labels) == 'list') && (typeof(yat) == 'list')) {
+		y.relation <- 'free';
+		}
+	if ((typeof(xaxis.labels) == 'list') && (typeof(xat) == 'list')) {
+		x.relation <- 'free';
+		}
+
+	# if user asked to retrieve previous plot labels, then the relations need to be free
+	if (is.logical(retrieve.plot.labels) && retrieve.plot.labels == TRUE) {
+		y.relation <- 'free';
+		x.relation <- 'free';
+		}
+
+	# if the limits, at and axis labels are not specified then carry them forward from the plots
+	if(is.null(xlimits)) {
+		xlimits <- if('same' == x.relation){ xlimits.plots[[plot.for.carry.over.when.same]]} else {xlimits.plots};
+		}
+	if(!is.null(xat) && !is.na(xat) && 1 == length(xat) && xat == TRUE) {
+		xat <- if('same' == x.relation){xat.plots[[plot.for.carry.over.when.same]]} else {xat.plots};
+		}
+	if(!is.null(xaxis.labels) && !is.na(xaxis.labels) && 1 == length(xaxis.labels) && xaxis.labels == TRUE) {
+		xaxis.labels <- if('same' == x.relation){xaxis.labels.plots[[plot.for.carry.over.when.same]]} else {xaxis.labels.plots};
+		}
+	if(is.null(ylimits)) {
+		ylimits <- if('same' == y.relation){ ylimits.plots[[plot.for.carry.over.when.same]]} else {ylimits.plots};
+		}
+	if(!is.null(yat) && !is.na(yat) && 1 == length(yat) && yat == TRUE) {
+		yat <- if('same' == y.relation){yat.plots[[plot.for.carry.over.when.same]]} else {yat.plots};
+		}
+	if(!is.null(yaxis.labels) && !is.na(yaxis.labels) && 1 == length(yaxis.labels) && yaxis.labels == TRUE) {
+		yaxis.labels <- if('same' == y.relation){yaxis.labels.plots[[plot.for.carry.over.when.same]]} else {yaxis.labels.plots};
+		}
+
+	# consolidate all the parameters together for updating the lattice 
 	x.scale <- list(
 		alternating = xaxis.alternating,
 		tck = xaxis.tck,
@@ -46,13 +107,9 @@ create.multiplot <- function(plot.objects, filename = NULL, panel.heights = c(1,
 		at = xat,
 		rot = xaxis.rot,
 		limits = xlimits,
-		fontface = if ('Nature' == style){'plain'} else(xaxis.fontface)
+		fontface = if ('Nature' == style){'plain'} else(xaxis.fontface),
+		relation = x.relation
 		);
-	# specify tck marks for different alternating settings
-	if (0 == yaxis.alternating) { yaxis.tck <- c(0,0); }
-	else if (1 == yaxis.alternating) { yaxis.tck[2] <- 0; }
-	else if (2 == yaxis.alternating) { yaxis.tck[1] <- 0; }
-
 	y.scale <- list(
 		alternating = yaxis.alternating,
 		tck = yaxis.tck,
@@ -61,60 +118,9 @@ create.multiplot <- function(plot.objects, filename = NULL, panel.heights = c(1,
 		at = yat,
 		rot = 0,
 		limits = ylimits,
-		fontface = if ('Nature' == style){'plain'} else(yaxis.fontface)
+		fontface = if ('Nature' == style){'plain'} else(yaxis.fontface),
+		relation = y.relation
 		);
-
-	# if user asked to retrieve previous plot labels
-	if (is.logical(retrieve.plot.labels) && retrieve.plot.labels == TRUE) {
-		y.relation <- 'free'
-		x.relation <- 'free'
-		y.scale$relation <- y.relation;
-		x.scale$relation <- x.relation;
-		}
-
-	y.same <- FALSE;
-
-	if ((typeof(yaxis.labels) != 'list') && (typeof(yat) != 'list') && (length(ylab.label) < 2)) {
-
-		if (0 == yaxis.alternating) { yaxis.tck <- c(0,0); }
-		else if (1 == yaxis.alternating) { yaxis.tck[2] <- 0; }
-		else if (2 == yaxis.alternating) { yaxis.tck[1] <- 0; }
-
-		y.scale <- list(
-			alternating = yaxis.alternating,
-			relation = y.relation,	
-			tck = yaxis.tck,
-			rot = 0,
-			labels = yaxis.labels,
-			cex = yaxis.cex,
-			at = yat,
-			limits = ylimits,
-			fontface = if ('Nature' == style){'plain'} else(yaxis.fontface)
-			);
-
-		y.same <- TRUE;
-		}
-	else{
-		y.scale$relation = 'free';
-		}
-
-	if (y.same && (typeof(xaxis.labels) != 'list') && (typeof(xat) != 'list') && (length(xlab.label) < 2)) {
-
-		x.scale <- list(
-			alternating = xaxis.alternating,
-			relation = x.relation,
-			tck = xaxis.tck,
-			labels = xaxis.labels,
-			cex = xaxis.cex,
-			at = xat,
-			limits = xlimits,
-			rot = xaxis.rot,
-			fontface = if ('Nature' == style){'plain'} else(xaxis.fontface)
-			);
-		}
-	else{
-		x.scale$relation = 'free';
-		}
 
 	trellis.object <- update(
 		combined.plot.objects,
@@ -150,18 +156,18 @@ create.multiplot <- function(plot.objects, filename = NULL, panel.heights = c(1,
 				cex = xlab.cex
 				)
 			),
-                xlab.top = BoutrosLab.plotting.general::get.defaults(
-                        property = 'fontfamily',
-                        add.to.list = list(
-                                label = xlab.top.label,
-                                cex = xlab.top.cex,
-                                col = xlab.top.col,
-                                fontface = if('Nature' == style){'plain'}else{'bold'},
-                                just = xlab.top.just,
-                                x = xlab.top.x,
+		xlab.top = BoutrosLab.plotting.general::get.defaults(
+			property = 'fontfamily',
+			add.to.list = list(
+				label = xlab.top.label,
+				cex = xlab.top.cex,
+				col = xlab.top.col,
+				fontface = if('Nature' == style){'plain'}else{'bold'},
+				just = xlab.top.just,
+				x = xlab.top.x,
 				y = xlab.top.y
-                                )
-                        ),
+				)
+			),
 		ylab = BoutrosLab.plotting.general::get.defaults(
 			property = 'fontfamily', 
 			add.to.list = list(
@@ -198,7 +204,7 @@ create.multiplot <- function(plot.objects, filename = NULL, panel.heights = c(1,
 				key.ylab.padding = 0.5,
 				ylab = 1,
 				ylab.axis.padding = ylab.padding,
-                		xlab.axis.padding = xlab.padding,
+				xlab.axis.padding = xlab.padding,
 				axis.left = 1,
 				axis.panel = 0.3,
 				strip.left = 0.3,
@@ -213,63 +219,78 @@ create.multiplot <- function(plot.objects, filename = NULL, panel.heights = c(1,
 		key = key,
 		legend = if (print.new.legend) {legend} else {combined.plot.objects$legend}
 		);
-        if (is.logical(retrieve.plot.labels) && retrieve.plot.labels == TRUE) {
-			Nxaxis.labels = list();
-			Nyaxis.labels = list();
-			Nxat = list();
-			Nyat = list();
-			Nxlimits = list();
-			Nylimits = list();
-			
-			for (p in c(1:length(plot.objects))) {
-				if(is.null(plot.labels.to.retrieve) || p %in% plot.labels.to.retrieve){
-					Nxlimits[[p]] = plot.objects[[p]]$x.limits;
-                                	Nxat[[p]] = plot.objects[[p]]$x.scales$at;
-                                	Nylimits[[p]] = plot.objects[[p]]$y.limits;
-                                	Nyat[[p]] = plot.objects[[p]]$y.scales$at;
-   					if (length(plot.objects[[p]]$x.scales$labels) > 0) {
-   						Nxaxis.labels[[p]] = plot.objects[[p]]$x.scales$labels;
-   						}
-					else { Nxaxis.labels[[p]] = TRUE; }
-   					if (length(plot.objects[[p]]$y.scales$labels) > 0) {
-   						Nyaxis.labels[[p]] = plot.objects[[p]]$y.scales$labels;
-   						}
-					else { Nyaxis.labels[[p]] = TRUE; }
-					}
-				else if(!is.null(plot.labels.to.retrieve)){
-					Nxlimits[[p]] = xlimits[[p]];
-					Nxat[[p]] = xat[[p]];
-					Nylimits[[p]] = ylimits[[p]];
-					Nyat[[p]] = yat[[p]];
-                                        if (length(plot.objects[[p]]$x.scales$labels) > 0) {
-                                                Nxaxis.labels[[p]] = plot.objects[[p]]$x.scales$labels;
-                                                }
-                                        else { Nxaxis.labels[[p]] = TRUE; }
-                                        if (length(plot.objects[[p]]$y.scales$labels) > 0) {
-                                                Nyaxis.labels[[p]] = plot.objects[[p]]$y.scales$labels;
-                                                }
-                                        else { Nyaxis.labels[[p]] = TRUE; }
-                                        }
 
-				
+	# update above doesn't seem to go through so force it here
+	trellis.object$x.limits = xlimits;
+	trellis.object$y.limits = ylimits;
+
+	# pulling forward a combination of axis limits, at and labels from the individual plots and the values passed to multiplot as a argument to the created mutliplot
+	if (is.logical(retrieve.plot.labels) && retrieve.plot.labels == TRUE) {
+		Nxaxis.labels <- list();
+		Nyaxis.labels <- list();
+		Nxat <- list();
+		Nyat <- list();
+		Nxlimits <- list();
+		Nylimits <- list();
+
+		for (p in c(1:length(plot.objects))) {
+			# if the plot is listed in plot.labels.to.retrieve, use the values from the individual plots
+			if(is.null(plot.labels.to.retrieve) || p %in% plot.labels.to.retrieve){
+				Nxlimits[[p]] <- plot.objects[[p]]$x.limits;
+				Nxat[[p]] <- plot.objects[[p]]$x.scales$at;
+				Nylimits[[p]] <- plot.objects[[p]]$y.limits;
+				Nyat[[p]] <- plot.objects[[p]]$y.scales$at;
+				if (length(plot.objects[[p]]$x.scales$labels) > 0) {
+					Nxaxis.labels[[p]] <- plot.objects[[p]]$x.scales$labels;
+					}
+				else { 
+					Nxaxis.labels[[p]] <- TRUE; 
+					}
+				if (length(plot.objects[[p]]$y.scales$labels) > 0) {
+					Nyaxis.labels[[p]] <- plot.objects[[p]]$y.scales$labels;
+					}
+				else { 
+					Nyaxis.labels[[p]] <- TRUE; 
+					}
 				}
-			trellis.object$x.limits = Nxlimits;
-			trellis.object$y.limits = Nylimits;
-			trellis.object$x.scales$at = Nxat;
-			trellis.object$y.scales$at = Nyat;
-			trellis.object$x.scales$labels = Nxaxis.labels;
-			trellis.object$y.scales$labels = Nyaxis.labels;
+			# if the plot is not listed in plot.labels.to.retrieve, use the values pass to multiplot as arguments
+			else if(!is.null(plot.labels.to.retrieve)){
+				Nxlimits[[p]] <- xlimits[[p]];
+				Nxat[[p]] <- xat[[p]];
+				Nylimits[[p]] <- ylimits[[p]];
+				Nyat[[p]] <- yat[[p]];
+				if (length(plot.objects[[p]]$x.scales$labels) > 0) {
+					Nxaxis.labels[[p]] <- plot.objects[[p]]$x.scales$labels;
+					}
+				else {
+					Nxaxis.labels[[p]] <- TRUE;
+					}
+				if (length(plot.objects[[p]]$y.scales$labels) > 0) {
+					Nyaxis.labels[[p]] <- plot.objects[[p]]$y.scales$labels;
+					}
+				else {
+					Nyaxis.labels[[p]] = TRUE;
+					}
+				}
+
 			}
-    # There is a glitch in update.trellis that prevents us from declaring multiple 'inside' legends
-    # To get around this, we'll add in a special case to just set the 'legend' manually
-    if (sum(names(legend) == "inside", na.rm = TRUE) > 1) {
-        trellis.object$legend = legend;
-        }
-   
+		trellis.object$x.limits = Nxlimits;
+		trellis.object$y.limits = Nylimits;
+		trellis.object$x.scales$at = Nxat;
+		trellis.object$y.scales$at = Nyat;
+		trellis.object$x.scales$labels = Nxaxis.labels;
+		trellis.object$y.scales$labels = Nyaxis.labels;
+		}
+
+	# There is a glitch in update.trellis that prevents us from declaring multiple 'inside' legends
+	# To get around this, we'll add in a special case to just set the 'legend' manually
+	if (sum(names(legend) == "inside", na.rm = TRUE) > 1) {
+		trellis.object$legend <- legend;
+		} 
     
-    # If flag set to TRUE for removing all border lines, reset panel border lines
-    # TODO: RSUN, allow custom setting to redraw certain border lines
-    if (remove.all.border.lines) {
+	# If flag set to TRUE for removing all border lines, reset panel border lines
+	# TODO: RSUN, allow custom setting to redraw certain border lines
+	if (remove.all.border.lines) {
        		trellis.object <- update(
        			trellis.object,
        			reference = FALSE,
@@ -286,7 +307,7 @@ create.multiplot <- function(plot.objects, filename = NULL, panel.heights = c(1,
 			)
        		}	
     
-    # If Nature style requested, change figure accordingly
+	# If Nature style requested, change figure accordingly
 	if ('Nature' == style) {
 
 		# Re-add bottom and left axes
@@ -309,12 +330,10 @@ create.multiplot <- function(plot.objects, filename = NULL, panel.heights = c(1,
 		warning("Nature also requires italicized single-letter variables and en-dashes for ranges and negatives. See example in documentation for how to do this.");
 
 		warning("Avoid red-green colour schemes, create TIFF files, do not outline the figure or legend")
-		} 
-
+		}
 	else if ('BoutrosLab' == style) {
 		# Nothing happens
 		}
-
 	else {
 		warning("The style parameter only accepts 'Nature' or 'BoutrosLab'.");
 		}
