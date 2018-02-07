f841519d653b0eadc782aee4d6848da7197fb3e8
jgreen <jgreen@noemail.ucla.edu>
2018-02-07 17:52:06 +0000

    <jrc-1>test R-CRTIC severity
    git-svn-id: https://code.boutroslab.oicr.on.ca/svn/BoutrosLab@69569 aa8168d8-c5e5-4185-88a0-f3e152ed764a

--- R/create.scatterplot.R
+++ R/create.scatterplot.R
@@ -197,7 +197,6 @@ create.scatterplot <- function(
 	regions.alpha = 1, lollipop.bar.y = NULL, lollipop.bar.color = 'gray',  ...
 	) {
 
-
 	### needed to copy in case using variable to define rectangles dimensions
         rectangle.info <- list(
         	xright = xright.rectangle,
