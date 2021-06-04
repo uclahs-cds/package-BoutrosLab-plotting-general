e9e1a2829db039dfca6aabadae125863882ab167
Chase Bakkeby <cbakkeby@mednet.ucla.edu>
2021-06-04 13:46:34 -0700

    Added dot feature to legend as an alternative to the default rectangles

--- R/legend.grob.R
+++ R/legend.grob.R
@@ -205,12 +205,23 @@ legend.grob <- function(
 							just = c('left', 'top'),
 							between = 0.5,
 							rep = FALSE,
-							rectangles = list(
-								col = legendi[['colours']],
-								size = if (is.null(legendi[['size']])) { size } else { legendi[['size']] },
-								height = 1,
-								border = legendi[['border']]
-								),
+							if (typei %in% c('rect', 'legend')) {
+							    legend.key$rectangles <- list(
+							        col = legendi[["colours"]], 
+							        size = if (is.null(legendi[["size"]]))  size else legendi[["size"]],
+							        height = 1,
+							        border = legendi[["border"]]
+							    )
+							} else if ('point' == typei) {
+							    legend.key$points <- list(
+							        col = if (!is.null(legendi[['col']])) legendi[['col']] else legendi[["colours"]], 
+							        cex = if (is.null(legendi[["cex"]])) size else legendi[["cex"]],
+							        fill = legendi[['fill']],
+							        pch = if (is.null(legendi[['pch']])) 19 else legendi[['pch']]
+							    )
+							} else {
+							    stop('type ', typei, ' unknown')
+							}
 							text = list(
 								legendi[['labels']],
 								cex = label.cex,
