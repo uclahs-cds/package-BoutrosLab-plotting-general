f28d07ed1679b1a85aead19289555ac8feee2369
Dan Knight <danknight@mednet.ucla.edu>
2023-10-09 10:44:58 -0700

    Add deprecation warning for old test format

---
+++ tests/test_plotting_functions.R
@@ -0,0 +1,5 @@
+warning(paste(
+    'BPG has been updated to use testthat.',
+    'To run the package tests, run "devtools::test()".',
+    'For more information, see https://testthat.r-lib.org/'
+    ));
