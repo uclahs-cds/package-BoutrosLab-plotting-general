f19960758f1dca08854697b6a20a98a62e7324f4
Dan Knight <danknight@mednet.ucla.edu>
2023-10-09 11:19:32 -0700

    Remove test deprecation warning

--- tests/test_plotting_functions.R
+++
@@ -1,5 +0,0 @@
-warning(paste(
-    'BPG has been updated to use testthat.',
-    'To run the package tests, run "devtools::test()".',
-    'For more information, see https://testthat.r-lib.org/'
-    ));
