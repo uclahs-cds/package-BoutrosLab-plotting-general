da718a074df1ede5dc663ed047b7519197b4258b
Dan Knight <danknight@mednet.ucla.edu>
2023-10-10 17:44:34 -0700

    Fix code style

--- tests/testthat.R
+++ tests/testthat.R
@@ -6,7 +6,7 @@
 # * https://r-pkgs.org/testing-design.html#sec-tests-files-overview
 # * https://testthat.r-lib.org/articles/special-files.html
 
-library(testthat)
-library(BoutrosLab.plotting.general)
+library(testthat);
+library(BoutrosLab.plotting.general);
 
-test_check("BoutrosLab.plotting.general")
+test_check('BoutrosLab.plotting.general');
