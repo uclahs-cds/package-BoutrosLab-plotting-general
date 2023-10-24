9b543a5c397a5ba6484ac78cf12d067a992f3c5b
Dan Knight <56950335+dan-knight@users.noreply.github.com>
2023-10-24 15:41:55 -0700

    Fix docstring
    Co-authored-by: Takafumi Yamaguchi <62572751+tyamaguchi-ucla@users.noreply.github.com>

--- R/auto.axis.R
+++ R/auto.axis.R
@@ -1,4 +1,4 @@
-#SMC.Benchmarking package is copyright (c) 2014 Ontario Institute for Cancer Research (OICR)
+# The BoutrosLab.plotting.general package is copyright (c) 2014 Ontario Institute for Cancer Research (OICR)
 # This package and its accompanying libraries is free software; you can redistribute it and/or modify it under the terms of the GPL
 # (either version 1, or at your option, any later version) or the Artistic License 2.0.  Refer to LICENSE for the full license text.
 # OICR makes no representations whatsoever as to the SOFTWARE contained herein.  It is experimental in nature and is provided WITHOUT
