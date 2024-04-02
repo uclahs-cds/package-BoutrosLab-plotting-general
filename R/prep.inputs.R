378d36cf6f64c6600e26032e419f77ae55ab7668
Dan Knight <danknight@mednet.ucla.edu>
2024-04-02 09:51:59 -0700

    Prep single sample.input setting value

--- R/prep.inputs.R
+++ R/prep.inputs.R
@@ -1,4 +1,8 @@
 prep.sample.order <- function(sample.order) {
+    if (length((sample.order) == 1)) {
+        return(prep.sample.order.setting(sample.order));
+        }
+
     contains.na <- any(is.na(sample.order));
 
     if (is.null(sample.order) || contains.na) {
@@ -11,5 +15,17 @@ prep.sample.order <- function(sample.order) {
             }
         }
 
+    result <- prep.sample.order;
+    }
+
+prep.sample.order.setting <- function(sample.order) {
+    valid.values <- c(
+        'none',
+        'ascending',
+        'descending'
+    );
+    if (!(sample.order %in% valid.values)) {
+        stop(paste('Invalid "sample.order":', paste0('(', sample.order, ')')))
+        }
     return(sample.order);
     }
